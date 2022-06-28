3204afe8fff5cb422af33b775e493f079076f6db
DanLevyM <dan.levy.pro@gmail.com>
2022-06-28 22:30:36 +0200

    logout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,23 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase-config';
 
 const Navbar = () => {
   const {toggleModals} = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      navigate('/');
+    } catch (e) {
+      alert('For some reasons we cannot deconnect, please check your internet connexion and retry.');
+    }
+  };
 
   return (
     <nav className="navbar navbar-light bg-light px-4">
@@ -12,12 +26,16 @@ const Navbar = () => {
       </Link>
 
       <div>
-        <button className="btn btn-primary" onClick={() => {
+        <button
+          className="btn btn-primary" onClick={() => {
           toggleModals('signUp');
         }}>Sign up
         </button>
         <button className="btn btn-primary m-1">Sign in</button>
-        <button className="btn btn-danger m-1">Log out</button>
+        <button
+          onClick={logOut}
+          className="btn btn-danger m-1">Log out
+        </button>
       </div>
     </nav>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 const Home = () => {
+  const {currentUser} = useContext(UserContext);
+
   return (
     <div className="container p-5">
-      <h1 className="display-3 text-light">Hi, sign up / sign in</h1>
+      <h1 className="display-3 text-light">
+        {currentUser ? 'Welcome bud' : 'Hi, sign up / sign in'}</h1>
 
     </div>
 
